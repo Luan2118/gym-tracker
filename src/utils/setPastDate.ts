79e7a6f36f918eedd6e7bc2f1465f1c9e0270387
Luan <leluanvn21@seznam.cz>
2026-04-11 14:32:40 +0200

    refactor: convert setPastDate to TypeScript

--- src/utils/setPastDate.ts
+++ src/utils/setPastDate.ts
@@ -1,6 +1,4 @@
-export default function setPastDate(number) {
+export default function setPastDate(number: number): string {
   const today = new Date();
-
-
   return new Date((new Date().setDate(today.getDate() - number))).toISOString();
 }
