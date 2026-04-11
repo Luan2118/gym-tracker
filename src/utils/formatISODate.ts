e4e64ec2c0adbde31767bb91a56e559aebdc09aa
Luan <leluanvn21@seznam.cz>
2026-04-11 14:21:35 +0200

    refactor: convert formatISODate to TypeScript

--- src/utils/formatISODate.ts
+++ src/utils/formatISODate.ts
@@ -1,3 +1,6 @@
-export default function formatISODate(date) {
+
+
+
+export default function formatISODate(date: string): string {
   return `${date.substring(8, 10)}-${date.substring(5, 7)}-${date.substring(0, 4)}`
 }
