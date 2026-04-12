dcdfe052a20a5bb9bd215afe7ed56b8ee5672336
Luan <leluanvn21@seznam.cz>
2026-04-12 17:09:01 +0200

    refactor: convert getPaginationData to TypeScript

--- src/utils/getPaginationData.ts
+++ src/utils/getPaginationData.ts
@@ -1,17 +1,33 @@
-export default function getPaginationData(itemsPerPage, data, currentPage   ) {
+
+type getPaginationDataReturnData<T> = {
+  pageNumbers: number[]
+  paginatedData: T[]
+  totalPages: number
+}
+
+export default function getPaginationData<T>(itemsPerPage: number, data: T[], currentPage: number): getPaginationDataReturnData<T> {
+  console.log(typeof itemsPerPage)
+  console.log(data)
+  console.log(typeof currentPage)
+
   const totalPages = Math.max(1, Math.ceil(data.length / itemsPerPage))
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage
 
-  
+
   const startPage = Math.max(1, currentPage - 2);
   const endPage = Math.min(totalPages, currentPage + 2)
-  
+
   const pageNumbers = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
 
   const paginatedData = data.slice(startIndex, endIndex)
 
+  console.log({
+    pageNumbers,
+    paginatedData,
+    totalPages
+  })
   return {
     pageNumbers,
     paginatedData,
