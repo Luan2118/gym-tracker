d509dc16014485ccf6403d82e33be6286d907dc9
Luan <leluanvn21@seznam.cz>
2026-04-12 17:18:14 +0200

    refactor: remove debug logs

--- src/utils/getPaginationData.ts
+++ src/utils/getPaginationData.ts
@@ -6,15 +6,10 @@ type getPaginationDataReturnData<T> = {
 }
 
 export default function getPaginationData<T>(itemsPerPage: number, data: T[], currentPage: number): getPaginationDataReturnData<T> {
-  console.log(typeof itemsPerPage)
-  console.log(data)
-  console.log(typeof currentPage)
-
   const totalPages = Math.max(1, Math.ceil(data.length / itemsPerPage))
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage
-
 
   const startPage = Math.max(1, currentPage - 2);
   const endPage = Math.min(totalPages, currentPage + 2)
@@ -23,11 +18,6 @@ export default function getPaginationData<T>(itemsPerPage: number, data: T[], cu
 
   const paginatedData = data.slice(startIndex, endIndex)
 
-  console.log({
-    pageNumbers,
-    paginatedData,
-    totalPages
-  })
   return {
     pageNumbers,
     paginatedData,
