7286f4f74b1a66121c5db72c90c1aef1a35bacb1
Luan <leluanvn21@seznam.cz>
2026-04-12 17:42:14 +0200

    refactor: create shared app types

---
+++ src/types.ts
@@ -0,0 +1,57 @@
+// BodyWeight type
+export type BodyWeight = {
+  bw: number
+  id: string
+  date: string
+}
+
+// Base types
+export type BaseSet = {
+  id: string
+  weight: number
+  reps: number
+}
+
+export type BaseExercise<TSet> = {
+  exerciseName: string
+  exerciseId: string
+  images: string[]
+  sets: TSet[]
+}
+
+// WorkoutHistory types
+export type WorkoutHistorySet = BaseSet & {
+  sessionId: string
+}
+
+export type WorkoutHistoryExercise =  BaseExercise<WorkoutHistorySet>
+
+export type WorkoutHistory = {
+  id: string
+  trainingSplitName: string
+  workoutDay: string
+  date: string
+  exercises: WorkoutHistoryExercise[]
+  duration: number
+}
+
+// TrainingSplit types
+export type TrainingSplitSet = BaseSet
+
+export type TrainingSplitExercise =  BaseExercise<TrainingSplitSet> & {
+  rowId: string
+  searchText: string
+}
+
+export type TrainingSplitWorkoutDay = {
+  id: string
+  name: string
+  confirm: boolean
+  exercises: TrainingSplitExercise[]
+}
+
+export type TrainingSplit = {
+  id: string
+  name: string
+  workoutDays: TrainingSplitWorkoutDay[]
+}
